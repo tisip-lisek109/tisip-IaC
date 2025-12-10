25a4fa4011586c21e581aa69f082f29aa4fe9ed3
Tomasz <lis109@wp.pl>
2025-12-10 20:31:47 +0100

    test deploy dev env

--- module06/terraform-projects/rg/main.tf
+++ module06/terraform-projects/rg/main.tf
@@ -15,4 +15,17 @@ provider "azurerm" {
 resource "azurerm_resource_group" "fd-rg" {
   name     = var.rgname
   location = var.location
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+# Ny ressurs  – Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.fd-rg.name
+  location                 = azurerm_resource_group.fd-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
