b486ab7240199a3341d7e4077939f7d57f3c350b
Tomasz <lis109@wp.pl>
2025-12-10 13:02:12 +0100

    add variables and tfvars to rg folder

---
+++ module06/terraform-projects/rg/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "rgname" {
+  description = "Name of the Resource Group"
+  type        = string
+}
